94d34dd286459cef0c9fbba5272e8716fadded6f
RiznykUliana <riznyk.uliana.a@gmail.com>
2018-06-15 15:27:36 +0300

    deleting some columns from players

---
+++ database/migrations/2018_06_15_122815_delete_some_fields_from_players.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteSomeFieldsFromPlayers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('alive_test')->table('players', function (Blueprint $table) {
+            $table->dropColumn('MaximumHP');
+            $table->dropColumn('MaximumPower');
+            $table->dropColumn('MaximumSatiety');
+            $table->dropColumn('FoodDecrement');
+            $table->dropColumn('CurrentFoodDecrement');
+            $table->dropColumn('MaximumThirst');
+            $table->dropColumn('WaterDecrement');
+            $table->dropColumn('CurrentWaterDecrement');
+            $table->dropColumn('NextLevelExperience');
+            $table->dropColumn('AttackPower');
+            $table->dropColumn('CurrentAttackPower');
+            $table->dropColumn('AttackSpeed');
+            $table->dropColumn('CurrentAttackSpeed');
+            $table->dropColumn('MovementSpeed');
+            $table->dropColumn('CurrentMovementSpeed');
+            $table->dropColumn('Armor');
+            $table->dropColumn('CurrentArmor');
+            $table->dropColumn('AttackDistance');
+            $table->dropColumn('CollectDistance');
+            $table->dropColumn('CurrentAttackDistance');
+            $table->dropColumn('DetectEnemyRadius');
+            $table->dropColumn('DetectResourceRadius');
+            $table->dropColumn('AttackBonus');
+            $table->dropColumn('GatheringBonus');
+            $table->dropColumn('CraftTimeImprovementBonus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
+
+
+
