d8dc3f6c93480c7e2f502553ae7988b5a9c6e18e
RiznykUliana <riznyk.uliana.a@gmail.com>
2018-06-18 18:27:15 +0300

    merging only for megaadmins

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,22 +11,37 @@
                         {{ session('status') }}
                     </div>
                 @endif
-                <div class="card">
-                    <div class="card-header">Merging</div>
+                @if(Auth::user()->role)
+                    <div class="card">
+                        <div class="card-header">Merging</div>
 
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-10 offset-md-1">
-                                <div class="row">
-                                    <a href="{{ url('test-to-production') }}" class="merge col-md-4 btn btn-danger" style="color: #fff">Merge test to production</a>
-                                    <a href="{{ url('production-to-test') }}" class="merge col-md-4 offset-md-3 btn btn-success" style="color: #fff">Merge production to test</a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-10 offset-md-1">
+                                    <div class="row">
+                                        <a href="{{ url('test-to-production') }}" class="merge col-md-4 btn btn-danger"
+                                           style="color: #fff">Merge test to production</a>
+                                        <a href="{{ url('production-to-test') }}"
+                                           class="merge col-md-4 offset-md-3 btn btn-success" style="color: #fff">Merge
+                                            production to test</a>
+                                    </div>
+
                                 </div>
 
                             </div>
-
                         </div>
                     </div>
-                </div>
+                @else
+                        <div class="card">
+                            <div class="card-header">Dashboard</div>
+
+                            <div class="card-body">
+                                <div class="row">
+
+                                </div>
+                            </div>
+                        </div>
+                @endif
             </div>
         </div>
     </div>
