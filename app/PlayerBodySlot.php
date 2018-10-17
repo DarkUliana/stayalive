2a1d1973b81ddf2874d8a9c3e004c229afe3502a
Uliana <riznyk.uliana.a@gmail.com>
2018-10-17 16:21:13 +0300

    bug in player body

--- app/PlayerBodySlot.php
+++ app/PlayerBodySlot.php
@@ -9,4 +9,8 @@ class PlayerBodySlot extends Model
     protected $primaryKey = 'ID';
 
     protected $guarded = [];
+
+    protected $hidden = [
+        'ID', 'googleID', 'created_at', 'updated_at'
+    ];
 }
