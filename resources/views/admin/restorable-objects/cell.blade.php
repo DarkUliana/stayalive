743a2b11a96e06a53479dc5b90f615f4c23d748a
Uliana <riznyk.uliana.a@gmail.com>
2018-11-13 10:14:26 +0200

    bug in index

--- resources/views/admin/restorable-objects/cell.blade.php
+++ resources/views/admin/restorable-objects/cell.blade.php
@@ -4,10 +4,10 @@
     @endphp
     <div class="box {{ $count ? 'dark-border' : '' }}"
          data-id="{{ $item->ID }}" style="background: {{ $item->cell->color }}">
-        <input type="hidden" name="deckCells[{{ $floor->floorIndex . $loop->index }}][floor]"
+        <input type="hidden" name="deckCells[{{ $loop->index . $item->cellIndex}}][floor]"
                value="{{ $floor->floorIndex }}" {{ $count ? '' : 'disabled' }}>
 
-        <input type="hidden" name="deckCells[{{ $floor->floorIndex . $loop->index }}][index]"
+        <input type="hidden" name="deckCells[{{ $loop->index . $item->cellIndex }}][index]"
                value="{{ $item->cellIndex }}" {{ $count ? '' : 'disabled' }}>
 
         <div class="box-inner">
