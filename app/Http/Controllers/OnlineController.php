27e5e47c72e7dcd6986b774defa3f05eeecdfac3
Uliana <riznyk.uliana.a@gmail.com>
2018-12-25 13:22:00 +0200

    online

--- app/Http/Controllers/OnlineController.php
+++ app/Http/Controllers/OnlineController.php
@@ -14,26 +14,26 @@ class OnlineController extends Controller
 
             return response('Your request has no localID', 400);
         }
-        return response(true, 200);
-//        $playerID = getPlayerID($request->localID);
-//
-//        $online = Online::where(['playerID' => $playerID])->first();
-//
-//
-//        if (empty($online)) {
-//
-//            if (!isset($request->withoutTouch)) {
-//                Online::create(['playerID' => $playerID]);
-//            }
-//
-//            return response('false', 200);
-//        }
-//
-//        $return = $online->online;
-//
-//        if (!isset($request->withoutTouch)) {
-//            $online->touch();
-//        }
-//        return response($return, 200);
+
+        $playerID = getPlayerID($request->localID);
+
+        $online = Online::where(['playerID' => $playerID])->first();
+
+
+        if (empty($online)) {
+
+            if (!isset($request->withoutTouch)) {
+                Online::create(['playerID' => $playerID]);
+            }
+
+            return response('false', 200);
+        }
+
+        $return = $online->online;
+
+        if (!isset($request->withoutTouch)) {
+            $online->touch();
+        }
+        return response($return, 200);
     }
 }
